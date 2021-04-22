ffd51714608fb747cf0e21b115bc4bec9e0b4504
Mikey <mikerichantk@gmail.com>
2021-04-21 20:29:26 -0700

    logic
    logic

--- cs445ProjectCode/TCPServer.java
+++ cs445ProjectCode/TCPServer.java
@@ -14,12 +14,13 @@ public class TCPServer {
      */
     public static void main(String[] args) throws IOException {
         // hold message and reply
-        String clientMessage, serverReply;
+        String clientMessage, serverReply, pattyMessage;
         String[] arrayOfWords = new String[200];
         Arrays.fill(arrayOfWords, "INVALID");
         int currentWord = 0;
         boolean activeGame = true;
         boolean first = true;
+        int currentTurn = 0;
         try {
             // create socket connection to port 9999
             ServerSocket accepting = new ServerSocket(9999);
@@ -28,20 +29,34 @@ public class TCPServer {
             while(activeGame) {
         
                 Socket connectionSocket = accepting.accept();
-                BufferedReader clientIn = new BufferedReader(new InputStreamReader		(connectionSocket.getInputStream()));
-                
-        
+                Socket pattySocket = accepting.accept();
+
+                BufferedReader clientIn = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
                 DataOutputStream clientOut = new DataOutputStream(connectionSocket.getOutputStream());
+
+                BufferedReader pattyIn = new BufferedReader(new InputStreamReader(pattySocket.getInputStream()));
+                DataOutputStream pattyOut = new DataOutputStream(pattySocket.getOutputStream());
                 // clientOut.writeBytes("Welcome to the word-chain game! Please provide a word to start the game! NOTE: The max size a chain can be is 200 words. \n");
 
                 // get message from client and captilatize the letters
-                clientMessage = clientIn.readLine();
+                if(currentTurn == 0){
+                    clientMessage = clientIn.readLine();
+                    System.out.println("Here.");
+                }
+                else{
+                    clientMessage = pattyIn.readLine();
+                }
+
                 if(arrayOfWords[currentWord] == "INVALID" && first){
                     serverReply = "Congrats on starting the chain. Start next turn." + "\nWord: ";
                     clientOut.writeBytes(serverReply);
                     arrayOfWords[currentWord] = clientMessage;
                     currentWord = 0;
                     first = false;
+                    serverReply = "Player one has played the first word which is: " + clientMessage;
+                    pattyOut.writeBytes(serverReply);
+                    currentTurn = 1;
+                    
                 }
                 else if(arrayOfWords[currentWord] == "INVALID"){
                     System.out.println("Before: " + arrayOfWords[currentWord]);
@@ -58,6 +73,12 @@ public class TCPServer {
                     currentWord++;
                     // send client reply 
                     clientOut.writeBytes(serverReply);
+                    if(currentTurn == 0){
+                        currentTurn = 1;
+                    }
+                    else{
+                        currentTurn = 0;
+                    }
                 }
                 else if(clientMessage.equals(arrayOfWords[currentWord])){
                     System.out.println("Attempted Word: " + clientMessage);
