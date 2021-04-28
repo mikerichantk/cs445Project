015e1be9999a815d4ebf1dcba26124ae57009dea
Mikey <mikerichantk@gmail.com>
2021-04-28 10:29:31 -0700

    Final logic

--- cs445ProjectCode/TCPServer.java
+++ cs445ProjectCode/TCPServer.java
@@ -32,56 +32,46 @@ public class TCPServer {
             BufferedReader clientIn = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
             DataOutputStream clientOut = new DataOutputStream(connectionSocket.getOutputStream());
 
-            System.out.println("first.");
-
             BufferedReader pattyIn = new BufferedReader(new InputStreamReader(pattySocket.getInputStream()));
             DataOutputStream pattyOut = new DataOutputStream(pattySocket.getOutputStream());
 
 
-            System.out.println("Got both clients.");
-
             // wait for clients to make connections
             while(activeGame) {
-                
-                System.out.println("pre accept");
 
                 // clientOut.writeBytes("Welcome to the word-chain game! Please provide a word to start the game! NOTE: The max size a chain can be is 200 words. \n");
 
 
                 // get message from client and captilatize the letters
                 if(currentTurn == 0){
-                    System.out.println("entered if statement");
+                    System.out.println("Turn: Player 1");
                     clientMessage = clientIn.readLine();
-                    System.out.println("Here.");
                 }
                 else{
-                    System.out.println("entered else");
+                    System.out.println("Turn: Player 2");
                     clientMessage = pattyIn.readLine();
-                    System.out.println("finished else");
                 }
                 // only accessed by current turn 0
                 if(arrayOfWords[currentWord] == "INVALID" && first){
-                    System.out.println("entered first if statement");
-                    serverReply = "Congrats on starting the chain. Start next turn." + "\nWord: ";
-                    clientOut.writeBytes(serverReply);
+                   // System.out.println("entered first if statement");
+                    //serverReply = "Congrats on starting the chain. Start next turn." + "\nWord: ";
+                  //  clientOut.writeBytes(serverReply);
                     arrayOfWords[currentWord] = clientMessage;
                     currentWord = 0;
                     first = false;
-                    serverReply = "Player one has played the first word which is: " + clientMessage;
-                    System.out.println("patty print 1");
-                    pattyOut.writeBytes(serverReply);
-                    System.out.println("patty print 2");
+                   // serverReply = "Player one has played the first word which is: " + clientMessage;
+                   // pattyOut.writeBytes(serverReply);
                     currentTurn = 1;
                     
                 }
                 else if(arrayOfWords[currentWord] == "INVALID"){
-                    System.out.println("entered second if statement");
-                    System.out.println("Before: " + arrayOfWords[currentWord]);
-                    serverReply = "Good Job! New word added. Start next turn." + "\nWord: ";
+                    //System.out.println("entered second if statement");
+                    //System.out.println("Before: " + arrayOfWords[currentWord]);
+                    //serverReply = "Good Job! New word added. Start next turn." + "\nWord: ";
                     // send client reply 
-                    clientOut.writeBytes(serverReply);
+                   // clientOut.writeBytes(serverReply);
                     arrayOfWords[currentWord] = clientMessage;
-                    System.out.println("After: " + arrayOfWords[currentWord]);
+                   // System.out.println("After: " + arrayOfWords[currentWord]);
                     currentWord = 0;
                     if(currentTurn == 0){
                         currentTurn = 1;
@@ -91,26 +81,32 @@ public class TCPServer {
                     }
                 }
                 else if(clientMessage.equals(arrayOfWords[currentWord]) && arrayOfWords[currentWord+1] == "INVALID"){
-                    System.out.println("entered third if statement");
-                    System.out.println("Attempted Word: " + clientMessage);
-                    serverReply = "Round passed! Now, add a new word to the chain." + "\nWord: "; 
+                    //System.out.println("entered third if statement");
+                  //  System.out.println("Attempted Word: " + clientMessage);
+                  //  serverReply = "Round passed! Now, add a new word to the chain." + "\nWord: "; 
                     currentWord++;
                     // send client reply 
-                    clientOut.writeBytes(serverReply);
+                   // clientOut.writeBytes(serverReply);
                 }
                 else if(clientMessage.equals(arrayOfWords[currentWord])){
-                    System.out.println("entered fourth if statement");
-                    System.out.println("Attempted Word: " + clientMessage);
-                    serverReply = "Correct. Please provide the next word in the chain.\n"; 
+                   // System.out.println("entered fourth if statement");
+                   // System.out.println("Attempted Word: " + clientMessage);
+                   // serverReply = "Correct. Please provide the next word in the chain.\n"; 
                     currentWord++;
                     // send client reply 
-                    clientOut.writeBytes(serverReply);
+                    //clientOut.writeBytes(serverReply);
                 }
                 else {
-                    System.out.println("entered last if statement");
-                    serverReply = "G A M E : O V E R ! Your guess was incorrect. The correct word was: " + arrayOfWords[currentWord];
+                    //System.out.println("entered last if statement");
+                    //serverReply = "G A M E : O V E R ! Your guess was incorrect. The correct word was: " + arrayOfWords[currentWord];
                     // send client reply 
-                    clientOut.writeBytes(serverReply);
+                    if(currentTurn == 0){
+                        System.out.println("Player 2 wins!");
+                    }
+                    else{
+                        System.out.println("Player 1 wins!");
+                    }
+                   // clientOut.writeBytes(serverReply);
                     currentWord = 0;
                     activeGame = false;
             
